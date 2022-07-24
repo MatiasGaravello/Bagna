596a2144cd5d24d9a6237bfcf0fcc290fd503113
Carlos Garavello <matiasgaravello@gmail.com>
2022-07-23 22:07:27 -0300

    Implementa nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import './Navbar.css'
+
+const Navbar = ()=>{
+ return(
+    <nav className="Navbar">
+        <div>
+            <h1>BAGNA</h1>
+        </div>
+        <div>
+            <button>Carteras</button>
+            <button>Bolsos y Maletines</button>
+            <button>Mochilas</button>
+            <button>Riñoneras</button>
+            <button>Portanotebooks</button>
+            <button>Bandoleras</button>
+            <button>Cartucheras</button>
+            <button>Billeteras</button>
+            <button>Promociones</button>
+            <button>Clutches</button>
+        </div>
+    </nav>
+ )   
+}
+
+export default Navbar
