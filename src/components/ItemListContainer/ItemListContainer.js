e23f7397a878a414bea3fba859e68e721e0ada87
Carlos Garavello <matiasgaravello@gmail.com>
2022-08-20 19:05:11 -0300

    Implementa loader al mostrar ItemList

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -7,18 +7,33 @@ import { useParams } from 'react-router-dom'
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
 
+    const [loading, setLoading] = useState()
+
     const { categoryId } = useParams()
 
     useEffect(() => {
         const asynFunction = categoryId ? getProductByCategory : getProducts
 
+        setLoading(true)
+
         asynFunction(categoryId).then(products => {
             setProducts(products)
         }).catch(error => {
             console.log(error)
+        }).finally(() => {
+            setLoading(false)
         })
 
     }, [categoryId])
+
+    if (loading) {
+        return (
+            <button class="btn btn-dark mt-5" type="button" disabled>
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Loading...
+            </button>
+        )
+    }
 
     return (
         <>
