42d1eb0f232f7be15395f0ae3a789b7a38e461e9
Carlos Matías Garavello <matiasgaravello@gmail.com>
2022-09-22 21:07:05 -0300

    Actualiza stock y redirige a inicio

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,35 +1,101 @@
-import { addDoc, collection, updateDoc, doc } from "firebase/firestore"
+import { addDoc, collection, updateDoc, doc, getDoc, query, where, getDocs, documentId, writeBatch } from "firebase/firestore"
 import { db } from "../../services/firebase"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import CartContext from "../../context/CartContext"
+import { useNavigate } from "react-router"
 
 
 const Checkout = () => {
+    const [isLoading, setIsLoading] = useState(false)
 
-    const { cart, getQuantity, getTotal } = useContext(CartContext)
+    const [orderCreated, setOrderCreated] = useState(false)
+
+    const { cart, getQuantity, getTotal, clearCart } = useContext(CartContext)
+
+    const navigate = useNavigate()
 
     const totalQuantity = getQuantity()
 
     const total = getTotal()
 
-    const createOrder = () => {
-        const objOrder = {
-            buyer: {
-                firstName: 'Carlos',
-                lastName: 'Garavello',
-                phone: '2611234566',
-                address: 'Liniers 145'
-            },
-            items: cart,
-            totalQuantity,
-            total,
-            date: new Date()
+    const createOrder = async () => {
+        setIsLoading(true)
+
+        try {
+            const objOrder = {
+                buyer: {
+                    firstName: 'Carlos',
+                    lastName: 'Garavello',
+                    phone: '2611234566',
+                    address: 'Liniers 145'
+                },
+                items: cart,
+                totalQuantity,
+                total,
+                date: new Date()
+            }
+
+            const ids = cart.map(prod => prod.idProducto)
+            console.log(ids)
+
+            const productsRef = collection(db, 'products')
+
+            const productsAddedFromFirestore = await getDocs(query(productsRef, where(documentId(), 'in', ids)))
+
+            const { docs } = productsAddedFromFirestore
+
+            const outOfStock = []
+
+            const batch = writeBatch(db)
+
+            docs.forEach(doc => {
+                const dataDoc = doc.data()
+                const stockDb = dataDoc.stock
+
+                const productAddedToCart = cart.find(prod => prod.idProducto === doc.id)
+                const prodQuantity = productAddedToCart?.quantity
+
+                if (stockDb >= prodQuantity) {
+                    batch.update(doc.ref, { stock: stockDb - prodQuantity })
+                } else {
+                    outOfStock.push({ id: doc.id, ...dataDoc })
+                }
+            })
+
+            if (outOfStock.length === 0) {
+                await batch.commit()
+
+                const orderRef = collection(db, 'orders')
+                const orderAdded = await addDoc(orderRef, objOrder)
+
+                console.log(`El id de su orden es: ${orderAdded.id}`)
+
+                clearCart()
+
+                setOrderCreated(true)
+
+                setTimeout(() => {
+                    navigate('/')
+                }, 3000)
+
+            } else {
+                console.log('Hay productos que estan fuera de stock')
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
         }
 
-        // const orderRef = doc(db, 'products', 'jYA6ZYOUZGuo5DFjjUhN')
-        // updateDoc(orderRef, { stock: 100 }).then(response => {
-        //     console.log(response)
-        // })
+
+    }
+
+    if (isLoading) {
+        return <h1>Se esta generando tu orden...</h1>
+    }
+
+    if (orderCreated) {
+        return <h1>La orden fue creada correctamente, sera redirigido al listado de productos</h1>
     }
 
     return (
